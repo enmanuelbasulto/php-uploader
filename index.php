f45f9b074bc4b953fe5d776e6ed12d31bcf03d3a
Enmanuel Basulto Martínez <enmanuel.basulto@uic.cu>
2022-02-14 18:48:27 -0500

    Agregado bootstrap.

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <html>
     <head>
         <script src="assets/dropzone.min.js"></script>
-        <link rel="stylesheet" href="assets/dropzone.min.css" type="text/css" />    
+        <script src="assets/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="assets/dropzone.min.css" type="text/css" />
+        <link rel="stylesheet" href="assets/bootstrap.min.css" type="text/css" />
     </head>
     <body>
         <form action="upload.php" class="dropzone"></form>
