15c36108c47ac7a6a753c2ad720cf5d5e6d24d36
Enmanuel Basulto Martínez <enmanuel.basulto@uic.cu>
2022-02-14 18:27:14 -0500

    Commit inicial.

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <script src="assets/dropzone.min.js"></script>
+        <link rel="stylesheet" href="assets/dropzone.min.css" type="text/css" />    
+    </head>
+    <body>
+        <form action="upload.php" class="dropzone" id="ficheros"></form>
+    </body>
+</html>
