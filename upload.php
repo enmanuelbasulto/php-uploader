63e6ce99cae63825d7fa60138c958b5d6cc65a2b
Enmanuel Basulto Martínez <enmanuel.basulto@uic.cu>
2022-02-14 18:36:39 -0500

    Arreglando problemitas.

--- upload.php
+++ upload.php
@@ -1,14 +1,9 @@
 <?php
 $target_dir = "files/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$target_file = $target_dir.basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo("Ya existe un archivo con el mismo nombre.");
-    $uploadOk = 0;
-}
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk !== 0) {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
